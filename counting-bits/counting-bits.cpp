8ebe648d6292236b46a8087137133c0cda073951
Arijit <adas7626@gmail.com>
2021-08-27 17:43:04 +0530

    Time: 4 ms (89.30%), Space: 7.9 MB (44.52%) - LeetHub

---
+++ counting-bits/counting-bits.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> countBits(int num) 
+    {
+        vector<int> v(num+1, 0);
+        for(int i=1;i<= num; i++) v[i]=v[i/2]+i%2;
+        return v;
+    }
+};
