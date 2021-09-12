303dbd1f839caebce2c4477a81be83f2f6cb6b08
Arijit <adas7626@gmail.com>
2021-09-12 12:02:54 +0530

    Time: 9 ms (7.60%), Space: 13.6 MB (23.28%) - LeetHub

---
+++ maximum-binary-tree-ii/maximum-binary-tree-ii.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+        if(root && root->val>val){
+            root->right = insertIntoMaxTree(root->right,val);
+            return root;
+        } 
+        else{
+            TreeNode *newNode = new TreeNode(val);
+            newNode->left = root;
+            return newNode;
+        }
+    }
+};
