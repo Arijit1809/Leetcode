382864ec9d3be1458660ae3060202b2ffc4408c5
Arijit <adas7626@gmail.com>
2021-09-15 15:56:34 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (98.83%) - LeetHub

---
+++ print-binary-tree/print-binary-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution{
+public:
+    vector<vector<string>> printTree(TreeNode* root){
+        vector<vector<string>> res;
+        int depth = maxDepth(root);
+        int width = pow(2, depth+1) - 1;
+        vector<string> row (width, "");
+        for (int i=0; i <= depth; i++){
+            res.push_back(row);
+        }
+        fillTable(root, 0, 0, width-1, res);
+        return res;
+    }
+    int maxDepth(TreeNode* node){
+        if (!node) { return -1; }
+        
+        int leftDepth = -1;
+        if (node->left) { leftDepth = maxDepth(node->left); }
+        
+        int rightDepth = -1;
+        if (node->right) { rightDepth = maxDepth(node->right); }
+        
+        return 1 + max( leftDepth, rightDepth );
+    }
+    void fillTable(TreeNode* node, int depth, int start, int end, vector<vector<string>>& table){
+        if (!node) { return; }
+        
+        int mid = (int)(start+end)/2;
+        table[depth][mid] = to_string(node->val);
+        
+        if (node->left)  { fillTable(node->left,  depth+1, start, mid-1, table); }
+        if (node->right) { fillTable(node->right, depth+1, mid+1, end,   table); }
+    }
+};
