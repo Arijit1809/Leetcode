3a3e75baec545de076238fbdc6ec7c6ba723787c
Arijit <adas7626@gmail.com>
2021-10-22 12:47:15 +0530

    Time: 184 ms (70.61%), Space: 127.5 MB (24.95%) - LeetHub

---
+++ stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {};
+        for (int a: stones)
+            cnt[a % 3]++;
+        if (cnt[0] % 2 == 0)
+            return cnt[1] > 0 && cnt[2] > 0;
+        return abs(cnt[1] - cnt[2]) > 2;
+    }
+};
