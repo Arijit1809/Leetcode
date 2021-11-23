d609f963017b5e149674c5baa933a53135c32d0a
Arijit <adas7626@gmail.com>
2021-11-24 00:10:04 +0530

    Time: 36 ms (12.95%), Space: 7.1 MB (49.32%) - LeetHub

--- longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -8,16 +8,15 @@ public:
         int result = 0;
         for(int end = 0; end < n; end++){
             counts[s[end]-'A']++;
-            if(maxCharCount < counts[s[end]-'A']){
-                maxCharCount = counts[s[end]-'A'];
-            }
+            maxCharCount = max(maxCharCount,counts[s[end]-'A']);
             while(end-start-maxCharCount+1 > k){
                 counts[s[start]-'A']--;
                 start++;
                 for(int i = 0; i < 26; i++){
-                    if(maxCharCount < counts[i]){
-                        maxCharCount = counts[i];
-                    }
+                    // if(maxCharCount < counts[i]){
+                    //     maxCharCount = counts[i];
+                    // }
+                    maxCharCount = max(maxCharCount,counts[s[end]-'A']); 
                 }
             }
             result = max(result, end-start+1);
