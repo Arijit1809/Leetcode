d91cc719260fa525ef9e280581864c512ff467ad
Arijit <adas7626@gmail.com>
2021-11-28 16:07:30 +0530

    Time: 132 ms (79.45%), Space: 102.4 MB (17.35%) - LeetHub

---
+++ decode-xored-permutation/decode-xored-permutation.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& A) {
+        int n = A.size() + 1, a = 0;
+        for (int i = 0; i <= n; ++i) {
+            a ^= i;
+            if (i < n && i % 2 == 1) {
+                a ^= A[i];
+            }
+        }
+        vector<int> res = {a};
+        for (int a: A) {
+            res.push_back(res.back() ^ a);
+        }
+        return res;
+    }
+};
