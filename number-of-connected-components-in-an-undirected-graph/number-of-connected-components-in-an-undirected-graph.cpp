2bd15c60513afba0d3e22e5a64270771142c9976
Arijit <adas7626@gmail.com>
2021-12-11 22:36:46 +0530

    Time: 16 ms (90.88%), Space: 12 MB (95.08%) - LeetHub

---
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int find(vector<int> &representative, int vertex) {
+        if (vertex == representative[vertex]) {
+            return vertex;
+        }
+        
+        return representative[vertex] = find(representative, representative[vertex]);
+    }
+    
+    int combine(vector<int> &representative, vector<int> &size, int vertex1, int vertex2) {
+        vertex1 = find(representative, vertex1);
+        vertex2 = find(representative, vertex2);
+        
+        if (vertex1 == vertex2) {
+            return 0;
+        } else {
+            
+            if (size[vertex1] > size[vertex2]) {
+                size[vertex1] += size[vertex2];
+                representative[vertex2] = vertex1;
+            } else {
+                size[vertex2] += size[vertex1];
+                representative[vertex1] = vertex2;
+            }
+            return 1;
+        }
+    }
+
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<int> representative(n), size(n);
+        
+        for (int i = 0; i < n; i++) {
+            representative[i] = i;
+            size[i] = 1;
+        }
+        
+        int components = n;
+        for (int i = 0; i < edges.size(); i++) {
+            components -= combine(representative, size, edges[i][0], edges[i][1]);
+        }
+
+        return components;
+    }
+};
