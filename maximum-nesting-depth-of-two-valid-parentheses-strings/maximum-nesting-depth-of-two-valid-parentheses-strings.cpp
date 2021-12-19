5572fe2e6cc22d79dc6e17c30ad5fe450a7d4c12
Arijit <adas7626@gmail.com>
2021-12-19 16:25:04 +0530

    Time: 4 ms (71.04%), Space: 7.4 MB (67.76%) - LeetHub

---
+++ maximum-nesting-depth-of-two-valid-parentheses-strings/maximum-nesting-depth-of-two-valid-parentheses-strings.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> maxDepthAfterSplit(string seq) {
+        vector<int> res(seq.length());
+        for (int i = 0; i < seq.length(); ++i)
+            res[i] = i & 1 ^ (seq[i] == '(');
+        return res;
+    }
+};
