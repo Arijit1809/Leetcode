9365de35a76d90fa622eb401ec789c8d61ae1a3a
Arijit <adas7626@gmail.com>
2022-01-30 23:04:31 +0530

    Time: 306 ms (45.93%), Space: 25.4 MB (83.32%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotate(self, nums, k):
+        def numReverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        k, n = k % len(nums), len(nums)
+        if k:
+            numReverse(0, n - 1)
+            numReverse(0, k - 1)
+            numReverse(k, n - 1)
