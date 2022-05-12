c7d57978249e4be1e2599cd7ad454490bfe2ad6b
Arijit <adas7626@gmail.com>
2022-05-12 11:33:41 +0530

    Time: 4 ms (24.12%), Space: 6.4 MB (82.11%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
-    bool wordPattern(string pattern, string str) {
-    map<char, int> p2i;
-    map<string, int> w2i;
-    istringstream in(str);
-    int i = 0, n = pattern.size();
-    for (string word; in >> word; ++i) {
-        if (i == n || p2i[pattern[i]] != w2i[word])
-            return false;
-        p2i[pattern[i]] = w2i[word] = i + 1;
+    bool wordPattern(string pattern, string s) {
+        bool isFollow = true;
+        unordered_map<char, string> wordMatrix;
+        unordered_map<string, char> inverseWordMatrix;
+        int stringIndex = 0;
+        for(int i = 0;i < pattern.length(); i++){
+            string toMatch = "";
+            if(stringIndex>s.length()-1){
+                isFollow = false;
+                break;
+            }
+            while(s[stringIndex]!=' ' && stringIndex<s.length()){
+                toMatch+=s[stringIndex];
+                stringIndex++;
+            }
+            stringIndex++;
+            cout<<pattern[i]<<" "<<toMatch<<endl;
+            
+            //for the letter to word matching
+            if(wordMatrix.count(pattern[i])==0){
+                wordMatrix[pattern[i]] = toMatch;
+            }else{
+                if(wordMatrix[pattern[i]]!=toMatch) isFollow = false;
+            }
+            
+            //for the word to letter matching
+            if(inverseWordMatrix.count(toMatch)==0){
+                inverseWordMatrix[toMatch] = pattern[i];
+            }else{
+                if(inverseWordMatrix[toMatch]!=pattern[i]) isFollow = false;
+            }
+        }
+        if(stringIndex<s.length()-1) isFollow = false;
+        return isFollow;
     }
-    return i == n;
-}
 };
