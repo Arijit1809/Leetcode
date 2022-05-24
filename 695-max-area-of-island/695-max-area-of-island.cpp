c174c3183f1609c2ef60e4e56dff1a495d8f6980
Arijit <adas7626@gmail.com>
2022-05-24 19:46:22 +0530

    Time: 48 ms (11.68%), Space: 24.4 MB (22.75%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -7,27 +7,28 @@ public:
     int maxAreaOfIsland(vvi& grid) {
         int rows = grid.size();
         int columns = grid[0].size();
-        // vvi visited(rows,vector<int>(columns,0));
+        vvi visited(rows,vector<int>(columns,0));
         int area = 0;
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < columns; j++){
                 if(grid[i][j]==1){
-                    area = max(area,rec(grid, i, j));
+                    area = max(area,rec(grid, i, j, visited));
                 }
             }
         }
         return area;
     }
 
-    int rec(vvi &grid, int currRow, int currColumn){
+    int rec(vvi& grid, int currRow, int currColumn, vvi& visited){
         int rows = grid.size();
         int columns = grid[0].size();
         if(currRow < 0 || currRow == rows || currColumn == columns || currColumn <0) return 0;
-        if(grid[currRow][currColumn]==0) return 0;
+        if(grid[currRow][currColumn]==0 || visited[currRow][currColumn]==1) return 0;
+        //else if value of cell is 1 then =>
         int area = 1;
-        grid[currRow][currColumn] = 0;
-        for(int i = 0; i < dir.size(); i++){
-            area += rec(grid,currRow+dir[i][0],currColumn+dir[i][1]);
+        visited[currRow][currColumn] = 1;
+        for(int i = 0; i < dir.size(); i++){ //writing dir.size() so it can be improved further for other directions
+            area += rec(grid,currRow+dir[i][0],currColumn+dir[i][1],visited);
         }
         return area;
     }
