9199f7ed0627c20e1baf9dd00560054d7c2c9994
Arijit <adas7626@gmail.com>
2022-05-28 23:29:48 +0530

    Time: 8 ms (16.23%), Space: 51.1 MB (48.77%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+
+        // Ensure that n is at the last index
+        if (nums[nums.length-1] != nums.length) {
+            return nums.length;
+        }
+        // Ensure that 0 is at the first index
+        else if (nums[0] != 0) {
+            return 0;
+        }
+
+        // If we get here, then the missing number is on the range (0, n)
+        for (int i = 1; i < nums.length; i++) {
+            int expectedNum = nums[i-1] + 1;
+            if (nums[i] != expectedNum) {
+                return expectedNum;
+            }
+        }
+
+        // Array was not missing any numbers
+        return -1;
+    }
+}
