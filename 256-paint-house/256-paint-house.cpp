cbc608a9610f79971cbc1faa4ee61a8925baed63
Arijit <adas7626@gmail.com>
2022-06-05 13:45:15 +0530

    Time: 13 ms (45.96%), Space: 10.1 MB (40.56%) - LeetHub

--- 256-paint-house/256-paint-house.cpp
+++ 256-paint-house/256-paint-house.cpp
@@ -1,7 +1,5 @@
 typedef vector<vector<int>> vvi;
 class Solution {
-private:
-    // vvi dp;
 public:
     int minCost(vector<vector<int>>& costs) {
         int houses = costs.size();
@@ -21,18 +19,10 @@ public:
                 dp[j][i] = minCost;
             }
         }
-        return min({dp[houses][0],dp[houses][1],dp[houses][2]});
+        int ans = INT_MAX;
+        for(int i = 0;i < colors;i++){
+            ans = min(ans,dp[houses][i]);
+        }
+        return ans;
     }
-    // int rec2(vvi& costs,int index, int color){
-    //     // if(index<0) return 0; //initialised in tabulation
-    //     if(dp[index+1][color]!=-1) return dp[index+1][color];
-    //     int minCost = INT_MAX;
-    //     for(int i = 0; i < costs[0].size(); i++){ //putting this in the tabulation with variable k
-    //         if(i == color){
-    //             continue;
-    //         }
-    //         minCost = min(minCost, costs[index][i] + rec2(costs,index-1,i));
-    //     }
-    //     return dp[index+1][color] = minCost;
-    // }
 };
