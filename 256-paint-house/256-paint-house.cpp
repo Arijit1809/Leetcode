a9a4d2ad65252bdc7bc5b243b03927cc9d0c0033
Arijit <adas7626@gmail.com>
2022-06-05 13:27:57 +0530

    Time: 20 ms (15.01%), Space: 10.3 MB (18.87%) - LeetHub

--- 256-paint-house/256-paint-house.cpp
+++ 256-paint-house/256-paint-house.cpp
@@ -1,24 +1,32 @@
 typedef vector<vector<int>> vvi;
 class Solution {
-    private:
+private:
     vvi dp;
 public:
     int minCost(vector<vector<int>>& costs) {
         int houses = costs.size();
         int colors = costs[0].size();
         dp.resize(houses+1,vector<int>(colors,-1));
+        for(int i = 0; i< colors;i++){
+            dp[0][i] = 0;
+        }
         int minCost = INT_MAX;
         for(int i = 0;i<colors;i++){
             minCost = min(minCost,rec2(costs,houses-1,i));
+            // for(int j = 0; j < houses ; j++){ //this is new, this is replacing the recursion
+            //     for(int k = 0; k < colors; k++){
+            //         if(k==i) continue;
+            //         dp[j+1][k] = min(dp[j+1][k], )
+            //     }
+            // }
         }
         return minCost;
     }
-    
     int rec2(vvi& costs,int index, int color){
-        if(index<0) return 0;
+        // if(index<0) return 0; //initialised in tabulation
         if(dp[index+1][color]!=-1) return dp[index+1][color];
         int minCost = INT_MAX;
-        for(int i = 0; i < costs[0].size(); i++){
+        for(int i = 0; i < costs[0].size(); i++){ //putting this in the tabulation with variable k
             if(i == color){
                 continue;
             }
