651930d36464d5e2ddafe758fea8b1c4c3730e4b
Arijit <adas7626@gmail.com>
2022-06-05 13:16:42 +0530

    Time: 23 ms (9.50%), Space: 10.3 MB (18.87%) - LeetHub

--- 256-paint-house/256-paint-house.cpp
+++ 256-paint-house/256-paint-house.cpp
@@ -1,25 +1,29 @@
 typedef vector<vector<int>> vvi;
 class Solution {
-private:
+    private:
     vvi dp;
 public:
     int minCost(vector<vector<int>>& costs) {
         int houses = costs.size();
         int colors = costs[0].size();
-        dp.resize(houses+1,vector<int>(colors+1,-1));
-        return rec(costs,costs.size()-1,-1);
+        dp.resize(houses+1,vector<int>(colors,-1));
+        int minCost = INT_MAX;
+        for(int i = 0;i<colors;i++){
+            minCost = min(minCost,rec2(costs,houses-1,i));
+        }
+        return minCost;
     }
     
-    int rec(vvi& costs,int index, int color){
-        if(dp[index+1][color+1]!=-1) return dp[index+1][color+1];
-        if(index<0) return dp[index+1][color+1] = 0;
+    int rec2(vvi& costs,int index, int color){
+        if(index<0) return 0;
+        if(dp[index+1][color]!=-1) return dp[index+1][color];
         int minCost = INT_MAX;
         for(int i = 0; i < costs[0].size(); i++){
             if(i == color){
                 continue;
             }
-            minCost = min(minCost, costs[index][i] + rec(costs,index-1,i)); //i color was used to paint this house
+            minCost = min(minCost, costs[index][i] + rec2(costs,index-1,i)); //i color was used to paint this house
         }
-        return dp[index+1][color+1] = minCost;
+        return dp[index+1][color] = minCost;
     }
 };
