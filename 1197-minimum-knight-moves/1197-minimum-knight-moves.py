71faf2cbd9a076bc14e9abd4c2045835f8650499
Arijit <adas7626@gmail.com>
2022-06-06 22:12:42 +0530

    Time: 96 ms (80.41%), Space: 15.5 MB (86.64%) - LeetHub

---
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minKnightMoves(self, x: int, y: int) -> int:
+
+        @lru_cache(maxsize=None)
+        def dfs(x, y):
+            if x + y == 0:
+                # base case: (0, 0)
+                return 0
+            elif x + y == 2:
+                # base case: (1, 1), (0, 2), (2, 0)
+                return 2
+            else:
+                return min(dfs(abs(x - 1), abs(y - 2)), dfs(abs(x - 2), abs(y - 1))) + 1
+
+        return dfs(abs(x), abs(y))
