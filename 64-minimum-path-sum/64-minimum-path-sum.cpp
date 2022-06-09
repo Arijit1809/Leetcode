7b54f0ec43297ed85c6682843fd27444be44e348
Arijit <adas7626@gmail.com>
2022-06-09 17:14:24 +0530

    Time: 10 ms (78.60%), Space: 10.2 MB (28.61%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -2,22 +2,39 @@ typedef vector<vector<int>> vvi;
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        vvi memo(grid.size(),vector<int>(grid[0].size(),-1));
-        return dfs(grid,0,0,grid.size(),grid[0].size(),memo);
+        vvi dp(grid.size()+1,vector<int>(grid[0].size()+1,-1));
+        int n = grid.size(), m = grid[0].size();
+        
+        for(int i = n ; i >= 0; i--){
+            for(int j = m ;j>=0;j--){
+                if(i==n || j==m){ 
+                    dp[i][j] = INT_MAX;
+                    continue;
+                }else if(i==n-1 && j==m-1){
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
+                int goDown = dp[i+1][j];
+                int goRight = dp[i][j+1];
+                dp[i][j] = grid[i][j] + min(goRight,goDown);
+            }
+        }
+        return dp[0][0];
+        // return dfs(grid,0,0,grid.size(),grid[0].size(),memo);
+        
     }
     
-    int dfs(vvi& grid, int row, int col, int n, int m, vvi& memo){
-        if(row==n-1 && col==m-1){
-            return grid[n-1][m-1];
-        }
-        if(memo[row][col]!=-1) return memo[row][col];
-        int goDown = INT_MAX, goRight = INT_MAX;
-        if(row<n-1){
-            goDown = grid[row][col] + dfs(grid,row+1,col,n,m,memo);
-        }
-        if(col<m-1){
-            goRight = grid[row][col] + dfs(grid,row,col+1,n,m,memo);
-        }
-        return memo[row][col] = min(goDown,goRight);
-    }
+    // int dfs(vvi& grid, int row, int col, int n, int m, vvi& memo){
+    //     if(row==n || col == m){
+    //         return INT_MAX;
+    //     }
+    //     if(row==n-1 && col==m-1){
+    //         return grid[n-1][m-1];
+    //     }
+    //     if(memo[row][col]!=-1) return memo[row][col];
+    //     int goDown = INT_MAX, goRight = INT_MAX;
+    //     goDown =  dfs(grid,row+1,col,n,m,memo);
+    //     goRight =  dfs(grid,row,col+1,n,m,memo);
+    //     return memo[row][col] = grid[row][col] + min(goDown,goRight);
+    // }
 };
