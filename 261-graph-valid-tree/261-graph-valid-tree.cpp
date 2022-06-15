14168a940f3b2f35e92d5f385616099a5cf85912
Arijit <adas7626@gmail.com>
2022-06-15 10:48:27 +0530

    Time: 22 ms (69.30%), Space: 11.9 MB (95.83%) - LeetHub

---
+++ 261-graph-valid-tree/261-graph-valid-tree.cpp
@@ -0,0 +1,47 @@
+class UnionFind {
+    private:
+    vector<int> root;
+    public:
+    UnionFind(int n) : root(n) {
+        for(int i = 0 ; i< n ; i++){
+            root[i] = i;
+        }
+    }
+    int find(int node){
+        if(node == root[node]) return node;
+        else return root[node] = find(root[node]); //path compression
+    }
+    void unionSet(int node1, int node2){
+        int root1 = find(node1);
+        int root2 = find(node2);
+        if(root1!=root2){
+            root[root2] = root1;
+        }
+    }
+    bool connected(int node1, int node2){
+        return find(node1) == find(node2);
+    }
+};
+
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        UnionFind graphSet(n);
+        int noOfEdges = edges.size();
+        int noOfEdgesInTree = 0;
+        for(int i = 0; i < noOfEdges; i++){
+            int node1 = edges[i][0], node2 = edges[i][1];
+            if(graphSet.connected(node1, node2)){ //already in the same set and now wants to form another edge means its a cycle
+                return false; //false as it is a cycle
+            } else{
+                graphSet.unionSet(node1,node2);
+                noOfEdgesInTree++;
+            }
+        }
+        if(noOfEdgesInTree == n-1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
