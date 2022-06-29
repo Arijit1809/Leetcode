c81af26669a8a45410089a2119e5b1d7cbdb5e9c
Arijit <adas7626@gmail.com>
2022-06-29 19:47:43 +0530

    Time: 3 ms (77.07%), Space: 8.7 MB (41.40%) - LeetHub

--- 369-plus-one-linked-list/369-plus-one-linked-list.cpp
+++ 369-plus-one-linked-list/369-plus-one-linked-list.cpp
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* plusOne(ListNode* head) {
@@ -21,9 +11,8 @@ public:
     }
     int carryOver(ListNode* node){
         if(node->next==NULL){
-            node->val+=1;
-            int hasCarry = node->val/10;
-            node->val = node->val%10;
+            int hasCarry = (node->val+1)/10;
+            node->val = (node->val+1)%10;
             return hasCarry;
         }
         int valueOfThisNode = node->val;
