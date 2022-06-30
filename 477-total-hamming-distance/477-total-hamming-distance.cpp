7e0133707571aa627c7ce7a978f173b620ba3338
Arijit <adas7626@gmail.com>
2022-06-30 18:46:30 +0530

    Time: 74 ms (55.29%), Space: 19 MB (56.20%) - LeetHub

---
+++ 477-total-hamming-distance/477-total-hamming-distance.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  int totalHammingDistance(vector<int>& nums)
+{
+    if (nums.empty())
+        return 0;
+
+    int ans = 0, n = nums.size();
+    vector<int> cnt(32, 0);         // count of elements with a particular bit ON
+
+    for (auto num : nums) {         // loop over every element
+        int i = 0;
+        while (num > 0) {           // check every bit
+            cnt[i] += (num & 0x1);
+            num >>= 1;
+            i++;
+        }
+    }
+
+    for (auto&& k : cnt) {           // loop over every bit count
+        ans += k * (n - k);
+    }
+
+    return ans;
+}
+};
