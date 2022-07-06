2deb7f4cc88fc43ff75d3e3d6e5fd69e5192f4fc
Arijit <adas7626@gmail.com>
2022-07-06 22:51:17 +0530

    Time: 727 ms (61.34%), Space: 6.9 MB (64.68%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int distributeCookies(vector<int>& cookies, int k) {
         vector<int> distribution(k,0);
-        return rec(cookies,distribution,0,k);
+        int total_cookies = accumulate(cookies.begin(),cookies.end(),0);
+        int optimal_cookie_spread = total_cookies/k;
+        return rec(cookies,distribution,0,k,optimal_cookie_spread);
     }
     
-    int rec(vector<int>& cookies, vector<int>& distribution, int index, int k){
+    int rec(vector<int>& cookies, vector<int>& distribution, int index, int k,int p){
         if(index == cookies.size()){
             int max_cookies = *max_element(distribution.begin(),distribution.end());
             return max_cookies;
@@ -13,9 +15,11 @@ public:
         int num_of_cookies = cookies[index];
         int min_unfairness = INT_MAX;
         for(int i = 0; i<k;i++){
-            distribution[i]+=num_of_cookies;
-            min_unfairness = min(min_unfairness, rec(cookies,distribution,index+1,k));
-            distribution[i]-=num_of_cookies;
+            if(distribution[i]<p){
+                 distribution[i]+=num_of_cookies;
+                min_unfairness = min(min_unfairness, rec(cookies,distribution,index+1,k,p));
+                distribution[i]-=num_of_cookies;
+            }
         }
         return min_unfairness;
     }
