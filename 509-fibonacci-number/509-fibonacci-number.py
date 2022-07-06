583757b3e2d421250bab1a6fd37d883826610571
Arijit <adas7626@gmail.com>
2022-07-06 11:07:34 +0530

    Time: 47 ms (60.27%), Space: 13.9 MB (54.45%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def fib(self, N: int) -> int:
+        if N <= 1:
+            return N
+        
+        cache = [0] * (N + 1)
+        cache[1] = 1
+        for i in range(2, N + 1):
+            cache[i] = cache[i - 1] + cache[i - 2]
+
+        return cache[N]
