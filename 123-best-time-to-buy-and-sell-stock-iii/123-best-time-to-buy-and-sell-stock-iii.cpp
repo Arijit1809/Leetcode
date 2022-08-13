3238618b40cfb706fd80a108c77caa465fde6349
Arijit <adas7626@gmail.com>
2022-08-13 10:30:31 +0530

    Time: 1025 ms (27.09%), Space: 207 MB (32.80%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,9 +2,28 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int trans = 2;
-        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(2, vector<int>(trans+1, -1)));
-        
-        return rec(prices, 0, 0, trans, dp);
+        int len = prices.size();
+        vector<vector<vector<int>>> dp(len+1, vector<vector<int>>(2, vector<int>(trans+1, 0)));
+        //already trans = 0 wala dp sab 0 hai
+        for(int i = len-1; i>=0; i--){
+            for(int j = 0; j <= 1; j++){
+                for(int k = 1; k <= trans; k++){
+                    if(j == 0){
+                        int buy = dp[i+1][1][k] - prices[i];
+                        int notBuy = dp[i+1][0][k];
+                        
+                        dp[i][j][k] = max(buy, notBuy);
+                    }else{
+                        int sell = dp[i+1][0][k-1] + prices[i]; 
+                        int notSell = dp[i+1][1][k];
+                        
+                        dp[i][j][k] = max(sell, notSell);
+                    }
+                }
+            }
+        }
+        return dp[0][0][2];
+        // return rec(prices, 0, 0, trans, dp);
     }
     
     int rec(vector<int>& prices, int index, int prev, int trans, vector<vector<vector<int>>>& dp){
